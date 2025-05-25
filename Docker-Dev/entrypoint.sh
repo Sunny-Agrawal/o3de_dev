b404ff2a8d5e3eb7c4251466ed94ea9ea693c0cb
Sunny-Agrawal <sunny.agrawal@utexas.edu>
2025-05-25 16:05:49 -0500

    Checkpoint before larger modifications. Not functional

--- Docker-Dev/entrypoint.sh
+++ Docker-Dev/entrypoint.sh
@@ -9,12 +9,11 @@ export UID GID USERNAME
 
 echo "[entrypoint] Using UID=$UID GID=$GID USERNAME=$USERNAME"
 
-# Setup runtime dir for GUI apps (XDG)
-mkdir -p /run/user/$UID
-chown $USERNAME:$USERNAME /run/user/$UID
-chmod 7700 /run/user/$UID
-export XDG_RUNTIME_DIR=/run/user/$UID
-echo "[entrypoint] Set XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR"
+# Commented out XDG setup for headless build-only usage
+# mkdir -p /run/user/$UID
+# chmod 7700 /run/user/$UID
+# export XDG_RUNTIME_DIR=/run/user/$UID
+# echo "[entrypoint] Set XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR"
 
-# Launch shell as the non-root dev user
-exec su - $USERNAME
+# Start an interactive shell directly
+exec bash
